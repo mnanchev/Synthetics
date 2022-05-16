241d01323d754e652c2fa2e9a7b192438ca35dd6
dsa <dsa>
2022-05-16 21:39:58 +0000

    Edited app.py

--- lib/src/deployment/app.py
+++ lib/src/deployment/app.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+import json
 import logging
 import sys
 
@@ -21,14 +22,17 @@ except Exception as exception:
 
 def lambda_handler(event, context):
     data = deepcopy(event)
+    print("data", data)
+    body = json.loads(data['body'])
+    print(body)
     model = joblib.load("best_estimator_model_0.6318845577211395_sgc.pkl")
     try:
-        hrv = int(data["hrv"])
+        hrv = int(body["hrv"])
     except KeyError:
         logging.error("No hrv value provided.")
         sys.exit(1)
     try:
-        heart_rate = int(data["heart_rate"])
+        heart_rate = int(body["heart_rate"])
     except KeyError:
         logging.error("No heart rate found")
         sys.exit(1)
@@ -37,4 +41,5 @@ def lambda_handler(event, context):
     x["HeartRateVariabilitySDNN"] = [hrv]
     prediction = model.predict(x)
     prediction = prediction.tolist()
+    print("Prediction: ", prediction, x)
     return {'prediction': str([prediction[0], x]), 'statusCode': 200}
